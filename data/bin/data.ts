61519ed21f5896208f3d37d80ec43b465c69791a
William Berks <bill@berks.software>
2023-11-27 11:17:24 -0800

    Remove obsolete projects

--- data/bin/data.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { configs } from '../lib/configs';
-import { DatabaseStack } from '../lib/database-stack';
-
-const app = new cdk.App();
-new DatabaseStack(app, 'DatabaseStack1026', configs);
